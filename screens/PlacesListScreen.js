59116d7ae5cf24caa6d5b1aae7d8a84edfceb26b
Leonardo Marquezani <leomarquezani@gmail.com>
2019-10-09 18:16:18 -0300

    Adding list places

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
+import PlaceItem from '../components/PlaceItem';
 
 const PlacesListScreen = props => {
 
+  const places = useSelector(state => state.places.places);
+
   return (
-    <View>
-      <Text>Places List Screen</Text>
-    </View>
+    <FlatList 
+      data={places}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <PlaceItem 
+        image={null}
+        title={itemData.item.title}
+        address={null}
+        onSelect={() => {
+          props.navigation.navigate('PlaceDetail', {
+            placeTitle: itemData.item.title,
+            placeId: itemData.item.id
+          })
+        }}
+      />}
+    />
   );
 };
 
