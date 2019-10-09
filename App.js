9e4d423586c3654bb4bd4ea6f76840f532f27f6b
Leonardo Marquezani <leomarquezani@gmail.com>
2019-10-09 17:55:43 -0300

    Added Redux

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
 
 import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <View>
+    <Provider store={store}>
       <PlacesNavigator />
-    </View>
+    </Provider>
   );
 }
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TextInput, ScrollView, Button } from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as placesActions from '../store/places-action';
 
 const NewPlaceScreen = props => {
 
+  const dispatch = useDispatch();
+
+  const [titleValue, setTitleValue] = useState('');
+
+  const titleChangeHandler = (text) => {
+    setTitleValue(text);
+  };
+
+  const savePlacehandler = () => {
+    dispatch(placesActions.addPlace(titleValue));
+    props.navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput 
+          style={styles.textInput} 
+          onChangeText={titleChangeHandler}
+          title={titleValue}
+        />
+        <Button 
+          title="Save Place" 
+          color={Colors.primary} 
+          onPress={savePlacehandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 2,
+    paddingVertical: 4
+  }
 });
 
 NewPlaceScreen.navigationOptions = {
